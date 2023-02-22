bf443e925e5d7cdf690220a8a45bdce5a69b0f56
lbau7 <l.baumann@aon.at>
2023-02-22 16:08:44 +0100

    fix tests

--- tests/testthat/test-get_details.R
+++ tests/testthat/test-get_details.R
@@ -2,13 +2,13 @@ test_that("get_details works for bma", {
   design <- setup_bma(k = 3, p0 = 0.2)
   set.seed(20230222)
   res1 <- get_details(design = design, n = 20, p1 = c(0.2, 0.4, 0.4),
-    lambda = 0.95, pmp0 = 1, iter = 5000)
+    lambda = 0.95, pmp0 = 1, iter = 1000)
 
   res2 <- get_details(design = design, n = 20, p1 = c(0.3, 0.5, 0.5),
-    lambda = 0.95, pmp0 = 1, iter = 500)
+    lambda = 0.95, pmp0 = 1, iter = 100)
 
   res3 <- get_details(design = design, n = 20, p1 = c(0.5, 0.5, 0.5),
-    lambda = 0.95, pmp0 = 1, iter = 5000)
+    lambda = 0.95, pmp0 = 1, iter = 100)
 
   # Rejection probabilities are higher when p is higher
   expect_true(all(res2$Rejection_Probabilities > res1$Rejection_Probabilities))
