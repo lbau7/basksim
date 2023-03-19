0132e50d56e37a0780a4f2269325b6ae5113928e
lbau7 <l.baumann@aon.at>
2023-03-19 16:58:59 +0100

    fix documentation

--- man-roxygen/tuning_jsdgen.R
+++ man-roxygen/tuning_jsdgen.R
@@ -2,3 +2,5 @@
 #'   based on pairwise similarity.
 #' @param eps_all Tuning parameter that determines the amount of borrowing
 #'   based on overall heterogeneity.
+#' @param logbase Tuning parameter. The base of the logarithm that is used to
+#'   calculate the Jensen-Shannon divergence.
