1cb7adc3c9314c3cc587dafda65df371d3884ea0
Lukas D. Sauer <sauer@imbi.uni-heidelberg.de>
2023-04-17 14:11:15 +0200

    Adding FWER to get_details.fujikawa and small bug fix in toer()

--- R/operating_characteristics.R
+++ R/operating_characteristics.R
@@ -48,9 +48,9 @@ ecd <- function(design, n, p1, lambda, design_params = list(), iter = 1000,
 toer <- function(design, n, p1 = NULL, lambda, design_params = list(),
                  iter = 1000, data = NULL, ...) {
   if (is.null(p1)) p1 <- rep(design$p0, design$k)
-  res <- do.call(get_results, args = c(design = list(design), n = n,
-    p1 = list(p1), lambda = lambda, design_params, iter = iter, data = data,
-    ...))
+  res <- do.call(get_results, args = c(design = list(design), n = list(n),
+                 p1 = list(p1), lambda = list(lambda), design_params,
+                 iter = list(iter), data = data, ...))
   res_sel <- res[, p1 == design$p0, drop = FALSE]
   res_all <- apply(res_sel, 1, function(x) any(x == 1))
   mean(res_all)
