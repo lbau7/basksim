752f462c858b7f95e4075fd15f1f4d839d036f3c
Lukas D. Sauer <sauer@imbi.uni-heidelberg.de>
2023-06-30 11:20:03 +0200

    Adding pow function

--- R/operating_characteristics.R
+++ R/operating_characteristics.R
@@ -55,3 +55,33 @@ toer <- function(design, n, p1 = NULL, lambda, design_params = list(),
   res_all <- apply(res_sel, 1, function(x) any(x == 1))
   mean(res_all)
 }
+
+#' Calculate the Power for a Basket Trial Design
+#'
+#' @template design
+#' @template n
+#' @template p1
+#' @template lambda
+#' @template design_params
+#' @template iter
+#' @template data
+#' @template dotdotdot
+#'
+#' @return A numeric value.
+#' @export
+#'
+#' @examples
+#' # Example for a basket trial with Fujikawa's Design
+#' design <- setup_fujikawa(k = 3, p0 = 0.2)
+#' pow(design = design, n = 20, p1 = c(0.2, 0.5, 0.5), lambda = 0.95,
+#'   design_params = list(epsilon = 2, tau = 0), iter = 1000)
+pow <- function(design, n, p1 = NULL, lambda, design_params = list(),
+                 iter = 1000, data = NULL, ...) {
+  if (p1 == rep(design$p0, design$k)) stop("p1 can not be the global null hypothesis for calculating power")
+  res <- do.call(get_results, args = c(design = list(design), n = list(n),
+                                       p1 = list(p1), lambda = lambda, design_params,
+                                       iter = list(iter), data = data, ...))
+  res_sel <- res[, p1 != design$p0, drop = FALSE]
+  res_all <- apply(res_sel, 1, function(x) any(x == 1))
+  mean(res_all)
+}
