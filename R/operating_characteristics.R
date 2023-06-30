1eff70736c389d251019317149c2d1dfe8cc82df
Lukas D. Sauer <sauer@imbi.uni-heidelberg.de>
2023-06-30 12:54:20 +0200

    Combined power and toer function

--- R/operating_characteristics.R
+++ R/operating_characteristics.R
@@ -85,3 +85,35 @@ pow <- function(design, n, p1 = NULL, lambda, design_params = list(),
   res_all <- apply(res_sel, 1, function(x) any(x == 1))
   mean(res_all)
 }
+
+#' Calculate Power and Type I Error Rate for a Basket Trial Design
+#'
+#' @template design
+#' @template n
+#' @template p1_toer
+#' @template lambda
+#' @template design_params
+#' @template iter
+#' @template data
+#' @template dotdotdot
+#'
+#' @return A numeric value.
+#' @export
+#'
+#' @examples
+#' # Example for a basket trial with Fujikawa's Design
+#' design <- setup_fujikawa(k = 3, p0 = 0.2)
+#' pow_toer(design = design, n = 20, p1 = c(0.2, 0.5, 0.5), lambda = 0.95,
+#'   design_params = list(epsilon = 2, tau = 0), iter = 1000)
+pow_toer <- function(design, n, p1 = NULL, lambda, design_params = list(),
+                iter = 1000, data = NULL, ...) {
+  if (is.null(p1)) p1 <- rep(design$p0, design$k)
+  res <- do.call(get_results, args = c(design = list(design), n = list(n),
+                                       p1 = list(p1), lambda = lambda, design_params,
+                                       iter = list(iter), data = data, ...))
+  res_sel_pow <- res[, p1 != design$p0, drop = FALSE]
+  res_all_pow <- apply(res_sel, 1, function(x) any(x == 1))
+  res_sel_toer <- res[, p1 == design$p0, drop = FALSE]
+  res_all_toer <- apply(res_sel, 1, function(x) any(x == 1))
+  return(c(FWER = mean(res_all_toer), EWP = mean(res_all_pow)))
+}
