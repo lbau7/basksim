4edc41d3d641c021fffc8a62d4a48b141a0b00ef
Sabrina Schmitt <s.schmitt208@gmx.de>
2023-11-22 13:43:48 +0100

    1.Error in get_weights_cpp() fixed: unique_combs no longer contains duplicates
    2.get_weights_jsd() also adjusted

--- R/helper.R
+++ R/helper.R
@@ -46,7 +46,7 @@ get_weights_jsd <- function(design, n, epsilon, tau, logbase, ...) {
       n1 <- dim_act[1]
       n2 <- dim_act[2]
 
-      weight_mat <- matrix(0, nrow = n1+1, ncol = n2+1)
+      jsd_mat <- matrix(0, nrow = n1+1, ncol = n2+1)
 
       shape1_post1 <- design$shape1 + c(0:n1)
       shape1_post2 <- design$shape1 + c(0:n2)
@@ -65,10 +65,13 @@ get_weights_jsd <- function(design, n, epsilon, tau, logbase, ...) {
 
           kl_f <- stats::integrate(f, 0, 1)$value
           kl_g <- stats::integrate(g, 0, 1)$value
-          weight_mat[i, j] <- 0.5 * kl_f + 0.5 * kl_g
+          jsd_mat[i, j] <- 0.5 * kl_f + 0.5 * kl_g
 
         }
       }
+
+      weight_mat <- (1 - jsd_mat)^epsilon
+      weight_mat[weight_mat <= tau] <- 0
 
       weights_list[[l]] <- weight_mat
 
@@ -83,35 +86,7 @@ get_weights_jsd <- function(design, n, epsilon, tau, logbase, ...) {
 
 }
 
-# get_weights_jsd <- function(design, n, epsilon, tau, logbase, ...) {
-#   shape1_post <- design$shape1 + c(0:n)
-#   shape2_post <- design$shape2 + c(n:0)
-#   n_sum <- n + 1
-#
-#   p <- function(x) stats::dbeta(x, shape1_post[i], shape2_post[i])
-#   q <- function(x) stats::dbeta(x, shape1_post[j], shape2_post[j])
-#   m <- function(x) 0.5 * (p(x) + q(x))
-#   f <- function(x) p(x) * log(p(x) / m(x), base = logbase)
-#   g <- function(x) q(x) * log(q(x) / m(x), base = logbase)
-#   h <- function(x) 0.5 * f(x) + 0.5 * g(x)
-#   jsd_mat <- matrix(0, nrow = n_sum, ncol = n_sum)
-#   for (i in 1:n_sum) {
-#     for (j in i:n_sum) {
-#       if (i == j) {
-#         next
-#       } else {
-#         kl_f <- stats::integrate(f, 0, 1)$value
-#         kl_g <- stats::integrate(g, 0, 1)$value
-#         jsd_mat[i, j] <- 0.5 * kl_f + 0.5 * kl_g
-#       }
-#     }
-#   }
-#   jsd_mat <- jsd_mat + t(jsd_mat)
-#   weight_mat <- (1 - jsd_mat)^epsilon
-#   weight_mat[weight_mat <= tau] <- 0
-#
-#   weight_mat
-# }
+
 
 # Weight matrix with CPP weights
 get_weights_cpp <- function(n, tune_a = 1, tune_b = 1, ...) {
