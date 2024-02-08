45f53411cbc8de2caab80a8670f9c0a5eec62caa
Sabrina Schmitt <s.schmitt208@gmx.de>
2024-02-08 15:23:21 +0100

    unit test: n must either have length 1 or k

--- tests/testthat/test-get_results.R
+++ tests/testthat/test-get_results.R
@@ -1,23 +1,22 @@
 test_that("get_results works for bma", {
   design <- setup_bma(k = 3, p0 = 0.2)
   data <- get_data(k = 3, n = 10, p = c(0.2, 0.5, 0.5), iter = 110)
-  data1 <- get_data(k = 3, n = c(10,20), p = c(0.2, 0.5, 0.5), iter = 110)
 
   res1 <- get_results(design = design, n = 10, p1 = c(0.2, 0.5, 0.5),
     lambda = 0.95, pmp0 = 1, iter = 100)
   res2 <- get_results(design = design, n = 10, lambda = 0.95, pmp0 = 1,
     data = data, iter = 110)
-  # res3 <- get_results(design = design, n = c(10,15,20), lambda = 0.95, pmp0 = 1,
-  #   data = data1, iter = 110)
 
   expect_equal(dim(res1), c(100, 3))
   expect_equal(dim(res2), c(110, 3))
   expect_true(all(res1 %in% c(0, 1)))
   expect_true(all(res2 %in% c(0, 1)))
 
-  # expect_false(length(attributes(data1)$n) == 3)
   expect_error(get_results.bma(design = design, n = c(10,20), p1 = NULL,
                                lambda = 0.95, pmp0 = 1, data = NULL, iter = 110))
+  expect_error(get_results.bma(design = design, n = c(10,20,30,40), p1 = NULL,
+                               lambda = 0.95, pmp0 = 1, data = NULL, iter = 110))
+
 
 })
 
@@ -57,6 +56,11 @@ test_that("get_results works for bhm", {
   data <- get_data(k = 3, n = 10, p = c(0.2, 0.5, 0.5), iter = 10)
   expect_error(get_results(design = design, n = 10, lambda = 0.95,
     tau_scale = 1, data = data))
+
+  expect_error(get_results.bhm(design = design, n = c(10,20), p1 = NULL,
+                               lambda = 0.95, pmp0 = 1, data = NULL, iter = 110))
+  expect_error(get_results.bhm(design = design, n = c(10,20,30,40), p1 = NULL,
+                               lambda = 0.95, pmp0 = 1, data = NULL, iter = 110))
 })
 
 test_that("get_results works for exnex", {
@@ -80,6 +84,11 @@ test_that("get_results works for exnex", {
   data <- get_data(k = 3, n = 10, p = c(0.2, 0.5, 0.5), iter = 10)
   expect_error(get_results(design = design, n = 10, lambda = 0.95,
     tau_scale = 1, w = 0.5, data = data))
+
+  expect_error(get_results.exnex(design = design, n = c(10,20), p1 = NULL,
+                               lambda = 0.95, pmp0 = 1, data = NULL, iter = 110))
+  expect_error(get_results.exnex(design = design, n = c(10,20,30,40), p1 = NULL,
+                               lambda = 0.95, pmp0 = 1, data = NULL, iter = 110))
 })
 
 test_that("get_results works for fujikawa", {
@@ -95,6 +104,11 @@ test_that("get_results works for fujikawa", {
   expect_equal(dim(res2), c(110, 3))
   expect_true(all(res1 %in% c(0, 1)))
   expect_true(all(res2 %in% c(0, 1)))
+
+  expect_error(get_results.fujikawa(design = design, n = c(10,20), p1 = NULL,
+                               lambda = 0.95, pmp0 = 1, data = NULL, iter = 110))
+  expect_error(get_results.fujikawa(design = design, n = c(10,20,30,40), p1 = NULL,
+                               lambda = 0.95, pmp0 = 1, data = NULL, iter = 110))
 })
 
 test_that("get_results works for jsdgen", {
@@ -125,6 +139,11 @@ test_that("get_results works for cpp", {
   expect_equal(dim(res2), c(110, 3))
   expect_true(all(res1 %in% c(0, 1)))
   expect_true(all(res2 %in% c(0, 1)))
+
+  expect_error(get_results.cpp(design = design, n = c(10,20), p1 = NULL,
+                               lambda = 0.95, pmp0 = 1, data = NULL, iter = 110))
+  expect_error(get_results.cpp(design = design, n = c(10,20,30,40), p1 = NULL,
+                               lambda = 0.95, pmp0 = 1, data = NULL, iter = 110))
 })
 
 test_that("get_results works for cppgen", {
