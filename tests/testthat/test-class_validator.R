d6aa80cac6d035c24dfcccc44f0a41a78337a523
Sabrina Schmitt <s.schmitt208@gmx.de>
2024-02-08 13:14:01 +0100

    unit tests class.R

--- tests/testthat/test-class_validator.R
+++ tests/testthat/test-class_validator.R
@@ -8,6 +8,11 @@ test_that("setup_jsdgen works", {
   expect_s3_class(design, "jsdgen")
 })
 
+test_that("setup_app works", {
+  design <- setup_app(k = 3, p0 = 0.2)
+  expect_s3_class(design, "app")
+})
+
 test_that("setup_cpp works", {
   design <- setup_cpp(k = 3, p0 = 0.2)
   expect_s3_class(design, "cpp")
@@ -18,9 +23,16 @@ test_that("setup_cppgen works", {
   expect_s3_class(design, "cppgen")
 })
 
+test_that("setup_cpplim works", {
+  design <- setup_cpplim(k = 3, p0 = 0.2)
+  expect_s3_class(design, "cpplim")
+})
+
 test_that("errors in setup functions work", {
   expect_error(setup_fujikawa(k = 0, p0 = 0.2))
   expect_error(setup_jsdgen(k = 3, p0 = 1))
   expect_error(setup_cpp(k = 2.5, p0 = 0.5))
+  expect_error(setup_app(k = 3.5, p0 = 0.5))
   expect_error(setup_cppgen(k = 3, p0 = 0.2, shape1 = 0, shape2 = 0))
+  expect_error(setup_cpplim(k = 4, p0 = 0.2, shape1 = 0, shape2 = 0))
 })
