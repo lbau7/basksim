7a51685dfc5cd092a5c169f2c39e71fba9214aa2
Sabrina Schmitt <s.schmitt208@gmx.de>
2024-02-15 13:02:28 +0100

    test check_get_weights_cpp with unequal n

--- tests/testthat/test-helper.R
+++ tests/testthat/test-helper.R
@@ -15,51 +15,8 @@ test_that("check_scenarios works", {
 
 test_that("check_get_weights_cpp", {
 
-  # # check unique combs
-  # expect_equal(unique(t(apply(arrangements::combinations(k = 2, v = c(1,2,3)),1,sort))),
-  #              matrix(data = c(1,2,1,3,2,3), ncol=2, byrow = TRUE))
-
-  validate_get_weights_cpp_diffn <- function(n, tune_a = 1, tune_b = 1){
-
-    if(length(n) == 1){
-      n <- c(n,n)
-    }
-
-    # find all possible combinations
-    unique_combs <- unique(t(apply(arrangements::combinations(k = 2, v = n),1,sort)))
-
-    g <- function(s, a, b) {
-      1 / (1 + exp(a + b * log(s)))
-    }
-
-    weights_list <- list()
-
-    for(l in 1:dim(unique_combs)[1]){
-      dim_act <- unique_combs[l,]
-      n1 <- dim_act[1]
-      n2 <- dim_act[2]
-
-      weight_mat <- matrix(0, nrow = n1+1, ncol = n2+1)
-      r1 <- 0:n1
-      r2 <- 0:n2
-
-      for(i in 1:length(r1)){
-        for(j in 1:length(r2)){
-          vec1 <- rep(0:1, c(n1 - r1[i], r1[i]))
-          vec2 <- rep(0:1, c(n2 - r2[j], r2[j]))
-          ks <- suppressWarnings(stats::ks.test(vec1, vec2)$statistic)
-          s <- max(n1,n2)^(1/4) * ks
-          weight_mat[i, j] <- g(s = s, a = tune_a, b = tune_b)
-        }
-      }
-      weights_list[[l]] <- weight_mat
-    }
-
-    weights_list
-  }
-
-  expect_equal(list(get_weights_cpp(n = 20, tune_a = 1, tune_b = 1)),
-               validate_get_weights_cpp_diffn(n = 20, tune_a = 1, tune_b = 1))
+  expect_equal(get_weights_cpp(n = 5, tune_a = 1, tune_b = 1),
+               get_weights_cpp(n = c(5,5,6,7), tune_a = 1, tune_b = 1)[[1]])
 
 })
 
