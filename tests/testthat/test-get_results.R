7d09e1c4d798641aed49271a35f39fa8802253e9
Sabrina Schmitt <s.schmitt208@gmx.de>
2024-02-15 10:38:15 +0100

    tests for get_results completed

--- tests/testthat/test-get_results.R
+++ tests/testthat/test-get_results.R
@@ -165,41 +165,41 @@ test_that("get_results works for cppgen", {
 })
 
 test_that("get_results works for cpplim", {
-  # design <- setup_cpplim(k = 3, p0 = 0.2)
-  # data <- get_data(k = 3, n = 10, p = c(0.2, 0.5, 0.5), iter = 110)
-  #
-  # res1 <- get_results(design = design, n = 10, p1 = c(0.2, 0.5, 0.5),
-  #                     lambda = 0.95, tune_a = 1, tune_b = 1, iter = 100)
-  # res2 <- get_results(design = design, n = 10, lambda = 0.95, tune_a = 1,
-  #                     tune_b = 1, data = data, iter = 110)
-  #
-  # expect_equal(dim(res1), c(100, 3))
-  # expect_equal(dim(res2), c(110, 3))
-  # expect_true(all(res1 %in% c(0, 1)))
-  # expect_true(all(res2 %in% c(0, 1)))
-  #
-  # expect_error(get_results.cpplim(design = design, n = c(10,20), p1 = NULL,
-  #                              lambda = 0.95, pmp0 = 1, data = NULL, iter = 110))
-  # expect_error(get_results.cpplim(design = design, n = c(10,20,30,40), p1 = NULL,
-  #                              lambda = 0.95, pmp0 = 1, data = NULL, iter = 110))
+  design <- setup_cpplim(k = 3, p0 = 0.2)
+  data <- get_data(k = 3, n = 10, p = c(0.2, 0.5, 0.5), iter = 110)
+
+  res1 <- get_results(design = design, n = 10, p1 = c(0.2, 0.5, 0.5),
+                      lambda = 0.95, tune_a = 1, tune_b = 1, iter = 100)
+  res2 <- get_results(design = design, n = 10, lambda = 0.95, tune_a = 1,
+                      tune_b = 1, data = data, iter = 110)
+
+  expect_equal(dim(res1), c(100, 3))
+  expect_equal(dim(res2), c(110, 3))
+  expect_true(all(res1 %in% c(0, 1)))
+  expect_true(all(res2 %in% c(0, 1)))
+
+  expect_error(get_results.cpplim(design = design, n = c(10,20), p1 = NULL,
+                               lambda = 0.95, pmp0 = 1, data = NULL, iter = 110))
+  expect_error(get_results.cpplim(design = design, n = c(10,20,30,40), p1 = NULL,
+                               lambda = 0.95, pmp0 = 1, data = NULL, iter = 110))
 })
 
 test_that("get_results works for app", {
-  # design <- setup_app(k = 3, p0 = 0.2)
-  # data <- get_data(k = 3, n = 10, p = c(0.2, 0.5, 0.5), iter = 110)
-  #
-  # res1 <- get_results(design = design, n = 10, p1 = c(0.2, 0.5, 0.5),
-  #                     lambda = 0.95, tune_a = 1, tune_b = 1, iter = 100)
-  # res2 <- get_results(design = design, n = 10, lambda = 0.95, tune_a = 1,
-  #                     tune_b = 1, data = data, iter = 110)
-  #
-  # expect_equal(dim(res1), c(100, 3))
-  # expect_equal(dim(res2), c(110, 3))
-  # expect_true(all(res1 %in% c(0, 1)))
-  # expect_true(all(res2 %in% c(0, 1)))
-  #
-  # expect_error(get_results.app(design = design, n = c(10,20), p1 = NULL,
-  #                                 lambda = 0.95, pmp0 = 1, data = NULL, iter = 110))
-  # expect_error(get_results.app(design = design, n = c(10,20,30,40), p1 = NULL,
-  #                                 lambda = 0.95, pmp0 = 1, data = NULL, iter = 110))
+  design <- setup_app(k = 3, p0 = 0.2)
+  data <- get_data(k = 3, n = 10, p = c(0.2, 0.5, 0.5), iter = 110)
+
+  res1 <- get_results(design = design, n = 10, p1 = c(0.2, 0.5, 0.5),
+                      lambda = 0.95, tune_a = 1, tune_b = 1, iter = 100)
+  res2 <- get_results(design = design, n = 10, lambda = 0.95, tune_a = 1,
+                      tune_b = 1, data = data, iter = 110)
+
+  expect_equal(dim(res1), c(100, 3))
+  expect_equal(dim(res2), c(110, 3))
+  expect_true(all(res1 %in% c(0, 1)))
+  expect_true(all(res2 %in% c(0, 1)))
+
+  expect_error(get_results.app(design = design, n = c(10,20), p1 = NULL,
+                                  lambda = 0.95, pmp0 = 1, data = NULL, iter = 110))
+  expect_error(get_results.app(design = design, n = c(10,20,30,40), p1 = NULL,
+                                  lambda = 0.95, pmp0 = 1, data = NULL, iter = 110))
 })
