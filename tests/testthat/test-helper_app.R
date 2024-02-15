0de53e2fa48d8058cc5c350dd3902ef6f49aef4a
Sabrina Schmitt <s.schmitt208@gmx.de>
2024-02-15 08:49:49 +0100

    testing beta_borrow_app

--- tests/testthat/test-helper_app.R
+++ tests/testthat/test-helper_app.R
@@ -32,3 +32,23 @@ test_that("get_gamma works", {
 
 
 })
+
+
+test_that("beta_borrow_app works", {
+  set.seed(20230319)
+
+  # compare with CPP (w = 1, alpha_0 = 1, gamma = 0)
+  design_app <- setup_app(k = 3, p0 = 0.2)
+  design_cpp <- setup_cpp(k = 3, p0 = 0.2)
+
+  n <- 15
+  r <- rep(10,3)
+
+  weights <- get_weights_cpp(n = n, tune_a = 1, tune_b = 1)
+  alpha_0 <- get_alpha_0_app(design = design_app, n = n)
+
+  expect_equal(beta_borrow_app(design = design_app, n = n, r = r, alpha_0 = alpha_0),
+               beta_borrow_cpp(design = design_cpp, n = n, r = r, weights = weights))
+
+
+})
